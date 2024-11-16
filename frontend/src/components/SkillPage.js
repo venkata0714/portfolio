a9577391792a6423f166ea24be27360fb24d8919
Kartavya Singh <kartavya.singh17@yahoo.com>
2024-11-16 05:01:09 -0500

    Skills Page compete without data checkpoint if stuff goes wrong

--- frontend/src/components/SkillPage.js
+++ frontend/src/components/SkillPage.js
@@ -1,6 +1,8 @@
 import { React, Container, Row, Col } from "react";
 import "../styles/SkillPage.css";
+import { motion, AnimatePresence } from "framer-motion";
 import Carousel from "react-multi-carousel";
+import SpaceExplorer from "../assets/img/media/header-img.svg";
 import "react-multi-carousel/lib/styles.css";
 
 const responsive = {
@@ -27,23 +29,83 @@ function SkillPage() {
   return (
     <section className="skill-container" id="skills">
       <div className="skill-div">
-        {/* <Container>
-          <Row>
-            <Col>
-              <div className="skill-box">
-                <h2>Skills</h2>
-                <p>Here are my Skills</p>
-                <Carousel
-                  responsive={responsive}
-                  infinite={true}
-                  className="skill-slider"
-                >
-                  <div className="item"></div>
-                </Carousel>
-              </div>
-            </Col>
-          </Row>
-        </Container> */}
+        {/* <motion.img src={SpaceExplorer} alt="" className="space-explorer" /> */}
+        <div className="skill-box">
+          <h2 className="skill-heading">Skills</h2>
+          <p className="skill-paragraph">Here are my Skills</p>
+          <Carousel
+            responsive={responsive}
+            infinite={true}
+            className="skill-slider"
+          >
+            <div className="item">
+              <img
+                className="skill-image"
+                src="https://via.placeholder.com/150"
+                alt="Skill"
+              />
+              <h5 className="skill-title">Skill 1</h5>
+              <p className="skill-description">
+                This is a Skill Description for a Skilll
+              </p>
+            </div>
+            <div className="item">
+              <img
+                className="skill-image"
+                src="https://via.placeholder.com/150"
+                alt="Skill"
+              />
+              <h5 className="skill-title">Skill 2</h5>
+              <p className="skill-description">
+                This is a Skill Description for a Skilll
+              </p>
+            </div>
+            <div className="item">
+              <img
+                className="skill-image"
+                src="https://via.placeholder.com/150"
+                alt="Skill"
+              />
+              <h5 className="skill-title">Skill 3</h5>
+              <p className="skill-description">
+                This is a Skill Description for a Skilll
+              </p>
+            </div>
+            <div className="item">
+              <img
+                className="skill-image"
+                src="https://via.placeholder.com/150"
+                alt="Skill"
+              />
+              <h5 className="skill-title">Skill 4</h5>
+              <p className="skill-description">
+                This is a Skill Description for a Skilll
+              </p>
+            </div>
+            <div className="item">
+              <img
+                className="skill-image"
+                src="https://via.placeholder.com/150"
+                alt="Skill"
+              />
+              <h5 className="skill-title">Skill 5</h5>
+              <p className="skill-description">
+                This is a Skill Description for a Skilll
+              </p>
+            </div>
+            <div className="item">
+              <img
+                className="skill-image"
+                src="https://via.placeholder.com/150"
+                alt="Skill"
+              />
+              <h5 className="skill-title">Skill 6</h5>
+              <p className="skill-description">
+                This is a Skill Description for a Skilll
+              </p>
+            </div>
+          </Carousel>
+        </div>
       </div>
     </section>
   );
