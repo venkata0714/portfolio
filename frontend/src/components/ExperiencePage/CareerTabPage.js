6a561a7459e34d17729aa11f3017a96f8345fe02
Kartavya Singh <kartavya.singh17@yahoo.com>
2025-02-18 00:09:25 -0500

    small fix

--- frontend/src/components/ExperiencePage/CareerTabPage.js
+++ frontend/src/components/ExperiencePage/CareerTabPage.js
@@ -37,7 +37,7 @@ const CustomArrow = ({ direction, onClick, imgSrc, label }) => {
 };
 const CareerTabPage = ({ addTab }) => {
   const [experiences, setExperiences] = useState([]);
-  const [activeSlide, setActiveSlide] = useState(1);
+  const [activeSlide, setActiveSlide] = useState(0);
 
   useEffect(() => {
     const getExperiences = async () => {
